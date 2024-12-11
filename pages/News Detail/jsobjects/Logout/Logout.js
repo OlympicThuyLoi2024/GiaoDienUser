71e10e7f756dbc151f76f68fa26c14bf86fff631
Nguyen Le Trung Thanh <trungthanhcva2206@gmail.com>
2024-12-11 11:32:16 +0000

    Update log out

---
+++ pages/News Detail/jsobjects/Logout/Logout.js
@@ -0,0 +1,13 @@
+export default {
+    navigateBasedOnRole: () => {
+        let targetAppURL = '';
+                targetAppURL = `http://14.225.218.213:8080/app/admin/sign-in-674b2a581cc72f1a54257910?branch=main`
+        navigateTo(targetAppURL, 'SAME_WINDOW');
+    }
+}
+/**
+ * @Project GiaoDienUser
+ * @Author TT(trungthanhcva2206@gmail.com@gmail.com)
+ * @Copyright (C) 2024 CHK. All rights reserved
+ * @License GNU/GPL version 3.0
+ */
